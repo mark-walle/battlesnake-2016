aa52f25fcb5dd626cbf6517f62b55b91f489d0fe
jennifertigner <jennifertigner@gmail.com>
2016-02-20 14:52:53 -0800

    Added methods to avoid running into the wall or self

--- app/main.py
+++ app/main.py
@@ -1,5 +1,23 @@
+## Battlesnake - Medusa
+
 import bottle
 import os
+import math
+
+    # snake object:
+    #    "id": "1234-567890-123456-7890",
+    #    "name": "Well Documented Snake",
+    #    "status": "alive",
+    #    "message": "Moved north",
+    #    "taunt": "Let's rock!",
+    #    "age": 56,
+    #    "health": 83,
+    #    "coords": [ [1, 1], [1, 2], [2, 2] ],
+    #    "kills": 4,
+    #    "food": 12,
+    #    "gold": 2
+
+snakeid = 'f729b53e-3477-447d-b07e-c79d7e326c82'
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -41,8 +59,24 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    move_decision = 'north'
+    move_decision = ['north', 'east', 'south', 'west']
 
+    ourSnake = findSnake(data['snakes'])
+    
+
+    #returns coordinates [x, y] of nearest food
+    nearestFood = findNearestFood(ourSnake, data['food'])
+    
+    #returns a list of blocked directions that won't run us into a wall
+    wallAvoidance = avoidWall(ourSnake['coords'], data['height'], data['width'])
+    for direction in wallAvoidance:
+        move_decision.remove(direction)
+    
+    #returns a list of blocked directions that won't run us into ourselves
+    selfAvoidance = avoidSelf(ourSnake['coords'])
+    for direction in selfAvoidance:
+        move_decision.remove(direction)
+    
     # THIS IS THE DATA WE RECEIVE: 
     # {
     #     "game": "hairy-cheese",
@@ -59,9 +93,82 @@ def move():
     # }
 
     return {
-        'move': move_decision,
+        'move': move_decision[0],
         'taunt': 'MEDUSA ATTACK!'
     }
+    
+def findSnake(snakes):
+    for snake in snakes:
+        if snake['id'] == snakeid:
+            return snake
+
+# Return the coords [x, y] of where the nearest food is
+# Distances are calculated by calculateDistance method below
+def findNearestFood(snake, foodList):
+    nearestFood = foodList[0]
+    headLocation = snake['coords'][0]
+    for food in foodList[1:]:
+        if calculateDistance(food, headLocation) < calculateDistance(nearestFood, headLocation):
+            nearestFood = food
+    return nearestFood
+
+# Calculate the distance between two coords [x1, y1], [x2, y2]
+# Because diagonal is not allowed, distance formula is 
+# deltaX + deltaY
+# Input: two coordinates as a list of [x, y]
+# Output: integer distance between coords
+def calculateDistance(coord1, coord2):
+    return math.abs( (coord1[0] - coord2[0]) + (coord1[1] - coord2[1]) )
+
+# Input: list of coordinates corresponding to snake position
+# Output: list of directions that are blocked by a wall
+def avoidWall(coordinates, height, width):
+    blockedDirections = []
+    head = coordinates[0]
+    if head[0] < 3:
+        blockedDirections.add('west')
+    if head[0] > width - 3:
+        blockedDirections.add('east')
+    if head[1] < 3: 
+        blockedDirections.add('north')
+    if head[1] > height - 3:
+        blockedDirections.add('south')
+    return blockedDirections
+    
+# This is a snake object    
+
+# {   "name": "Well Documented Snake",
+#     "status": "alive",
+#     "message": "Moved north",
+#     "taunt": "Let's rock!",
+#     "age": 56,
+#     "health": 83,
+#     "coords": [ [1, 1], [1, 2], [2, 2] ],
+#     "kills": 4,
+#     "food": 12,
+#     "gold": 2
+# }
+
+# Input: list of coordinates corresponding to snake position
+# Output: list of blocked directions
+def avoidSelf(coordinates):
+    blockedDirections = []
+    
+    for body in coordinates[1:]:
+        if coordinates[0][0] == body[0]-1:
+            blockedDirections.append('east')
+        
+        if coordinates[0][0] == body[0]+1:
+            blockedDirections.append('west')
+        
+        if coordinates[0][1] == body[1]-1:
+            blockedDirections.append('north')
+        
+        if coordinates[0][1] == body[1]+1:
+            blockedDirections.append('south')
+        
+    return blockedDirections
+
 
 @bottle.post('/end')
 def end():
